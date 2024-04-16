c9bee332c43be4bfc7bab60359789e8b822ede3d
bingfeng.yang <bingfeng.yang@lotuscars.com.cn>
2024-04-16 16:40:21 +0800

    init

---
+++ packages/client/src/router/router.ts
@@ -0,0 +1,10 @@
+import {createRouter, createWebHistory} from 'vue-router';
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/Home.vue'),
+    },
+  ],
+});
